cb5126066ba409c264e8f47f63398f68914b2021
GPJAlameda <gpjalameda@gmail.com>
2014-03-25 22:16:01 +0100

    Animated Cuore

--- project_code/mar14.js
+++ project_code/mar14.js
@@ -3,9 +3,9 @@ $(function() {
     var w11 = new Wirelib($("#canvasHead")[0]);
     w11.strokeStyle = "rgb(255, 0, 255)";       
     w11.addCircle(0,0,0,70,4); 
-    w11.loop(54, function() {
+    w11.loop(64, function() {
         w11.rotateX(0.015);
-        //w13.rotateY(0.01);
+        w13.rotateY(0.01);
         w11.draw();
     });
     
@@ -14,23 +14,19 @@ $(function() {
     w13.addCircle(0,0,0,160,64);
     w13.loop(48, function() {
         w13.rotateY(0.01);
-        //w13.rotateY(0.01);
+        w13.rotateY(0.01);
         w13.draw();
     });
     
-    var w14 = new Wirelib($("#canvasEngine")[0]), n = 0;
-    w14.strokeStyle = "rgba(255, 0, 255, 0.5)";
-    w14.addBox(0, 0, 0, 100, 100, 100);
-    w14.loop(12,
-        function() {
-            w14.rotateX(0.1);
-            w14.rotateY(Math.sin(n) * 0.2);
-            n += 0.02;
-        }
-    );
+    //var w14 = new Wirelib($("#canvasEngine")[0]), n = 0;
+    //w14.strokeStyle = "rgba(255, 0, 255, 0.5)";
+    //w14.addBox(0, 0, 0, 100, 100, 100);
+    //w14.loop(12,
+    //    function() {
+    //        w14.rotateX(0.1);
+    //        w14.rotateY(Math.sin(n) * 0.2);
+    //        n += 0.02;
+    //    }
+    //);
     
-    var w12 = new Wirelib($("#canvasNeedle")[0]);
-    w12.strokeStyle = "rgb(255, 0, 255)";
-    w12.Cuore();              
-      
 });
---
+++ project_code/mar16.js
@@ -0,0 +1,131 @@
+$(function() {
+    var points = [], numPoints = 5, i, canvas, context, width, height, gravity = 0.02, emitter;
+    
+    canvas = $("#canvasFlyCuore")[0];
+    width = canvas.width;
+    height = canvas.height;
+    context = canvas.getContext("2d");
+    
+    emitter = {x:width / 2, y:height};
+       
+    function initPoint(p) {
+        p.x = emitter.x;
+        p.y = emitter.y;
+        p.vx = Math.random() * 4 - 2;
+        p.vy = Math.random() * -5 - 3;
+        p.width = Math.random() * 20 + 1;
+        p.height = p.width + 2;
+    }
+    
+    function update() {
+        var i, point, len = points.length;
+        for(i = 0; i < len; i += 1) {
+            point = points[i];
+            point.vy += gravity;
+            point.x += Math.sin(point.vx);
+            point.y += Math.cos(point.vy);
+            if(point.x > width ||
+               point.x < 0 ||
+               point.y > height ||
+               point.y < 0) {
+                initPoint(point);
+            }
+        }
+    }
+    
+    function draw() {
+        var i, point, len = points.length;
+        context.clearRect(0, 0, width, height);
+        for(i = 0; i < len; i += 1) {
+            point = points[i];                   
+            drawCuore(point.x,point.y,point.width,point.height);
+        }        
+    }
+    
+    
+    function drawCuore(cx,cy,cwidth,cheight) {
+        
+            var px, py, pxq, pyq, pxend, pyend;
+            
+            context.beginPath();            
+            
+            px = cx;
+            py = cy;
+            pyend = py - cheight/4;
+            context.moveTo(px, pyend);
+            
+            //1
+            pxend = px + cwidth/6;
+            pyend = py - cheight/2.7;
+            pxq = px + cwidth/14;
+            pyq = pyend;
+            context.quadraticCurveTo(pxq, pyq, pxend, pyend);
+    
+            //2
+            pxend = px + (cwidth/2.3);
+            pyend = py;
+            pxq = pxend;
+            pyq = py - (cheight/2.7);
+            context.quadraticCurveTo(pxq, pyq, pxend, pyend);        
+            
+            //3
+            pxend = px;
+            pyend = py + (cheight/2.5);
+            pxq = px + (cwidth/2.4);
+            pyq = pyend - (cheight/8);
+            context.quadraticCurveTo(pxq, pyq, pxend, pyend);
+    
+            //4
+            pxend = px - (cwidth/2.3);
+            pyend = py;
+            pxq = px - (cwidth/2.3);
+            pyq = pyend + (cheight/4);
+            context.quadraticCurveTo(pxq, pyq, pxend, pyend);
+            
+            //5
+            pxend = px - cwidth/6;
+            pyend = py - cheight/2.7;
+            pxq = px - cwidth/2.3;
+            pyq = pyend;        
+            context.quadraticCurveTo(pxq, pyq, pxend, pyend);
+            
+            //6
+            pxend = px;
+            pyend = py - cheight/4;
+            pxq = px - cwidth/14;
+            pyq = py - cheight/2.7;                
+            context.quadraticCurveTo(pxq, pyq, pxend, pyend);        
+            
+            context.lineWidth = 2;
+            context.strokeStyle = "rgb(255, 0, 255)";
+            context.fillStyle = "rgba(200, 0, 0, 0.8)";
+            context.stroke();
+            context.fill();
+        
+    };        
+       
+    function addPoint() {
+        var point;
+        if(points.length < numPoints) {
+            point = {};
+            initPoint(point);
+            points.push(point);
+        }          
+    }
+    
+    setInterval(function() {
+        addPoint();
+        update();
+        draw();
+    }, 1000/24);
+    
+    setInterval(function() {
+        var y;
+        y = 20 + Math.random() * 35 + 1;
+        drawCuore(emitter.x,emitter.y - y,40,40);
+        context.moveTo(emitter.x,emitter.y-98);
+        context.lineTo(emitter.x,emitter.y-45);
+        context.stroke();        
+    }, 1000/24);
+    
+});
